29937daabcbd380b4826f3e60c36b87f607a070b
Justus Dietrich <git@justus-d.de>
2024-01-24 22:39:36 +0100

    Shade plugin, tests, wrong class in MainApplication

--- src/main/java/de/justusd/filetopng/MainApplication.java
+++ src/main/java/de/justusd/filetopng/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("FileToPNG");
         stage.setScene(scene);
--- src/test/java/de/justusd/filetopng/service/FileToPNGTest.java
+++ src/test/java/de/justusd/filetopng/service/FileToPNGTest.java
@@ -2,6 +2,9 @@ package de.justusd.filetopng.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileToPNGTest {
@@ -18,6 +21,23 @@ class FileToPNGTest {
         assertEquals(0L, FileToPNG.contentLengthForPart(1_200_001_000L, 2));
         assertEquals(1_200_000_000L, FileToPNG.contentLengthForPart(1_200_001_000L * 2L + 1000L, 1));
         assertEquals(1_000L, FileToPNG.contentLengthForPart(1_200_000_000L * 2L + 1_000L, 2));
+    }
+
+    @Test
+    void messageDigest() throws NoSuchAlgorithmException, CloneNotSupportedException {
+        MessageDigest md1 = MessageDigest.getInstance("SHA-256");
+        MessageDigest md2 = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = new byte[255];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) i;
+        }
+        md1.update(bytes);
+        System.out.println(FileToPNG.byteToHexString(md1.digest()));
+        md2.update(bytes);
+        MessageDigest md2Clone = (MessageDigest) md2.clone();
+        System.out.println(FileToPNG.byteToHexString(md2Clone.digest()));
+        md2.update(bytes);
+        System.out.println(FileToPNG.byteToHexString(md2.digest()));
     }
 
 }
