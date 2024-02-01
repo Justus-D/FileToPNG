5886051cca8396aaa8dec1857a1fae82a66e3f03
Justus Dietrich <git@justus-d.de>
2024-02-01 02:07:40 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module de.justusd.filetopng {
-    requires javafx.controls;
-    requires javafx.fxml;
-//    requires pngj;
     requires static annotations;
     requires java.desktop;
     requires java.logging;
+
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
 
     opens de.justusd.filetopng to javafx.fxml;
     exports de.justusd.filetopng;
